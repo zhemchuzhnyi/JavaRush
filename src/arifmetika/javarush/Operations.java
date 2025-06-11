a426d03bafbcfce4d156e28036f341dea3fe8efe
Andrey_O <a_reshkin@inbox.ru>
2025-06-11 22:58:27 +0300

    Update Practice

--- src/arifmetika/javarush/Operations.java
+++ src/arifmetika/javarush/Operations.java
@@ -15,7 +15,7 @@ public class Operations {
         // Остаток от деления
 
         int x = 3333;
-        int y = 333 % 2;
+        int y = 33 % 2;
         System.out.println(y);
         /*
         В этом примере мы делим 33 на 2.
