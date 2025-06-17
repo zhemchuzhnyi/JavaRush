438024003de67e4351563c8e992b6d4139505b12
Andrey_O <a_reshkin@inbox.ru>
2025-06-17 23:16:13 +0300

    Update Practice

--- src/arifmetika/javarush/Prisvaivanie.java
+++ src/arifmetika/javarush/Prisvaivanie.java
@@ -5,7 +5,7 @@ public class Prisvaivanie {
     // сравнение 2х чисел
     public static void main(String[] args) {
 
-        int x = 313;
+        int x = 33;
         int y = 9199;
 
         System.out.println(x == y);
